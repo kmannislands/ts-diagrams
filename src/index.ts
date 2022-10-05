1c6cc151e191406356eaf7b6a4580b28bf3c4aba
Kieran Mann <kieran.mann@o8t.com>
2022-10-04 19:02:42 -0700

    add basic syntax example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-export const app = 'Hello';
+interface SequenceMessage {
+    from: string,
+    to: string,
+    label: string,
+
+    dotted?: boolean,
+}
+
+class SequenceDiagram {
+  public addActor(_actorName: string): SequenceDiagram {
+    return this;
+  }
+
+  public addMessage(_msg: SequenceMessage): SequenceDiagram {
+    return this;
+  }
+}
+
+
+// Example syntax:
+new SequenceDiagram()
+  .addActor("Alice")
+  .addActor("Bob")
+  .addMessage({ from: "Alice", to: "Bob", label: 'Authentication Request' })
+  .addMessage({ from: "Bob", to: "Alice", label: 'Authentication Response', dotted: true });
