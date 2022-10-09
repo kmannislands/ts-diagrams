e2f7b9190ee2250356565c9ea402178cc2c31c3b
Kieran Mann <kieran.mann@o8t.com>
2022-10-09 13:26:49 -0700

    write minimal code to make test pass

--- src/frontend-plantuml.ts
+++ src/frontend-plantuml.ts
@@ -1,5 +1,6 @@
 import { Diagram, DiagramEntityType } from "./diagram";
 import { SequenceDiagram, SequenceDiagramType } from "./sequence-diagram";
+import { Participant } from "./sequence-diagram/participant";
 import { assertUnreachable, BrandedStr } from "./type-util";
 
 type PlantUMLSource = BrandedStr<"PlantUMLSource">;
@@ -9,24 +10,50 @@ export function sequenceDiagramToPuml(
   title?: string
 ): PlantUMLSource {
   const titleFragment = `@startuml ${title || ""}`;
+  const participantAliases = new Map<Participant<string>, string>();
   const sourceStringFragments: string[] = [];
 
   // declare participants in order
   for (const participant of diagram.entities(DiagramEntityType.Participant)) {
-    const participantSource = `${participant.subType} "${participant.name}"`;
+    // TODO check for other illegal characters
+    const needsAlias = participant.name.indexOf(" ") > 0;
+
+    let alias = "";
+    if (needsAlias) {
+      // TODO how to efficiently determine the seq?
+      alias = `${participant.subType}1`;
+
+      participantAliases.set(participant, alias);
+    }
+
+    const participantSource = `${participant.subType} "${participant.name}"${
+      alias && ` as ${alias}`
+    }`;
     sourceStringFragments.push(participantSource);
   }
 
   // Add a line break between groups
-  sourceStringFragments.push('');
+  sourceStringFragments.push("");
 
   // declare messages in order
   for (const message of diagram.entities(DiagramEntityType.Message)) {
-    const msgSource = `${message.from} ${message.dotted ? "-->" : "->"} ${message.to} : ${message.label}`;
+    // Resolve any aliases
+    const fromParticipantName =
+      message.from instanceof Participant
+        ? participantAliases.get(message.from)
+        : message.from;
+    const toParticipantName =
+      message.to instanceof Participant
+        ? participantAliases.get(message.to)
+        : message.to;
+
+    const msgSource = `${fromParticipantName} ${
+      message.dotted ? "-->" : "->"
+    } ${toParticipantName} : ${message.label}`;
     sourceStringFragments.push(msgSource);
   }
 
-  sourceStringFragments.push('');
+  sourceStringFragments.push("");
 
   const closeUmlFragment = "@enduml";
 
--- src/sequence-diagram/sequence-message.ts
+++ src/sequence-diagram/sequence-message.ts
@@ -1,29 +1,34 @@
 import { DiagramEntity, DiagramEntityType } from "../diagram";
+import { Participant } from "./participant";
 
 export interface SequenceMessageConfig<ParticipantName extends string> {
-  from: ParticipantName;
-  to: ParticipantName;
+  from: ParticipantName | Participant<ParticipantName>;
+  to: ParticipantName | Participant<ParticipantName>;
   label?: string;
 
   // TODO separate style
   dotted?: boolean;
 }
 
-export class SequenceMessage<ParticipantName extends string> implements SequenceMessageConfig<ParticipantName>, DiagramEntity {
+export class SequenceMessage<ParticipantName extends string>
+  implements SequenceMessageConfig<ParticipantName>, DiagramEntity
+{
   public readonly type = DiagramEntityType.Message;
 
-  constructor(private readonly message: SequenceMessageConfig<ParticipantName>) {}
+  constructor(
+    private readonly message: SequenceMessageConfig<ParticipantName>
+  ) {}
 
-  get from(): ParticipantName {
+  get from(): ParticipantName | Participant<ParticipantName> {
     return this.message.from;
   }
 
-  get to(): ParticipantName {
+  get to(): ParticipantName | Participant<ParticipantName> {
     return this.message.to;
   }
 
   get label(): string {
-    return this.message.label || '';
+    return this.message.label || "";
   }
 
   get dotted(): boolean {
