7c4e6bea7d7220c0bfc3a9510d4a8772cbd25f01
Kieran Mann <kieran.mann@o8t.com>
2022-10-20 16:58:40 -0700

    remove dead ts-node

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
-export default {
+module.exports = {
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
 
